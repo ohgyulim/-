7fa43f87eb8a1a44fab2056a0137ed7eef66ccd4
gyulim5 <ohrbfla1004@khu.ac.kr>
2025-06-11 16:49:26 +0900

    [feat] 정책 카드 컴포넌트 구현(#2)

--- app/page.js
+++ app/page.js
@@ -1,6 +1,41 @@
 import BasicInfoForm from "./components/BasicInfoForm";
+import PolicyCard from "./components/PolicyCard";
 
 export default function Home() {
+  // Todo: 서버에서 가져오기
+  let policies = [
+    {
+      index: 1,
+      likes: false,
+      name: "정책1",
+      district: "광진구",
+      content: "11만원 지원함",
+      target: "만 19세 ~ 만 24세 이하",
+      startDate: "2024-01-01",
+      endDate: "2024-02-02",
+    },
+    {
+      index: 2,
+      likes: false,
+      name: "정책2",
+      district: "용산구",
+      content: "22만원 지원함",
+      target: "만 19세 ~ 만 24세 이하",
+      startDate: "2024-01-01",
+      endDate: "2024-02-02",
+    },
+    {
+      index: 3,
+      likes: false,
+      name: "정책2",
+      district: "종로구",
+      content: "33만원 지원함",
+      target: "만 19세 ~ 만 24세 이하",
+      startDate: "2024-01-01",
+      endDate: "2024-02-02",
+    },
+  ];
+
   return (
     <div>
       <section className="text-center py-8 px-4 bg-gray-50">
@@ -11,7 +46,15 @@ export default function Home() {
           내 조건에 맞는 현재 신청 가능한 정책을 찾아보세요
         </p>
       </section>
-      <BasicInfoForm />
+      <section className="bg-white p-10 rounded-2xl shadow-md max-w-5xl mx-auto mt-6 pb-30">
+        <BasicInfoForm />
+
+        <div className="flex pt-50 gap-10">
+          {policies.map((policy, i) => {
+            return <PolicyCard policyInfo={policy} key={i} />;
+          })}
+        </div>
+      </section>
     </div>
   );
 }
